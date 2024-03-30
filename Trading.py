2dabe1f8d8ab4ec1caa9606429aa4b23aab38b85
Michael Xie <mxie0114@glamil.com>
2024-03-30 13:51:19 -0400

    Added Functional Backtesting, testing trades

--- Trading.py
+++ Trading.py
@@ -14,3 +14,33 @@ ALPACA_CREDS = {
     #Switch to false if you would like to do non-paper trading
     "PAPER":True
 }
+
+class MLTrades(Strategy):
+    def initialize(self, symbol:str="SPY"):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+    def on_trading_iteration(self):
+        if self.last_trade == None:
+            order = self.create_order(
+                self.symbol,
+                10,
+                "buy",
+                type = "market"
+            )
+            self.submit_order(order)
+            self.last_trade = "buy"
+
+broker = Alpaca(ALPACA_CREDS)  
+strat = MLTrades(name='mlstrat', broker = broker, parameteres = {})
+
+#Back Testing Functions
+start_date = datetime(2023,12,15)
+end_date = datetime(2023,12,31)
+
+strat.backtest(
+    YahooDataBacktesting,
+    start_date,
+    end_date,
+    parameters={}
+)
